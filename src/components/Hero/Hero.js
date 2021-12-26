e459308a9452179aea39f7f298037f35684adf89
harryalbert <harryalbert364@gmail.com>
2021-12-25 20:32:24 -0500

    finished hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+	Section,
+	SectionText,
+	SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import {LeftSection} from "./HeroStyles";
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+	<Section row nopadding>
+		<LeftSection>
+			<SectionTitle main center>
+				Welcom to <br />
+				My Personal Portfolio
+			</SectionTitle>
+			<SectionText>
+				The purpose of Javascript Mastery is to help aspiring and
+				established developers to master web developement and to build
+				awesome apps
+			</SectionText>
+			<Button
+				onClick={() => {
+					window.location = "https://google.com";
+				}}
+			>
+				Learn More
+			</Button>
+		</LeftSection>
+	</Section>
 );
 
 export default Hero;
