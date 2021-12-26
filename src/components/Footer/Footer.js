e7e15ad20c5fe4bccfaddef109516266506e0937
harryalbert <harryalbert364@gmail.com>
2021-12-26 08:01:41 -0500

    finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import {AiFillGithub, AiFillInstagram, AiFillLinkedin} from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import {SocialIcons} from "../Header/HeaderStyles";
+import {
+	CompanyContainer,
+	FooterWrapper,
+	LinkColumn,
+	LinkItem,
+	LinkList,
+	LinkTitle,
+	Slogan,
+	SocialContainer,
+	SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
+	return (
+		<FooterWrapper>
+			<LinkList>
+				<LinkColumn>
+					<LinkTitle>Call</LinkTitle>
+					<LinkItem href="tel:413-770-6129">
+						tel: (413) 770-6129
+					</LinkItem>
+				</LinkColumn>
+				<LinkColumn>
+					<LinkTitle>Email</LinkTitle>
+					<LinkItem href="mailto:harryalbert364@gmail.com">
+						email: harryalbert364@gmail.com
+					</LinkItem>
+				</LinkColumn>
+			</LinkList>
+			<SocialIconsContainer>
+				<CompanyContainer>
+					<Slogan>Innovating one project at a time</Slogan>
+				</CompanyContainer>
+				<SocialContainer>
+					<SocialIcons href="https://github.com">
+						<AiFillGithub size="3rem" />
+					</SocialIcons>
+					<SocialIcons href="https://linkedin.com">
+						<AiFillLinkedin size="3rem" />
+					</SocialIcons>
+					<SocialIcons href="https://instagram.com">
+						<AiFillInstagram size="3rem" />
+					</SocialIcons>
+				</SocialContainer>
+			</SocialIconsContainer>
+		</FooterWrapper>
+	);
 };
 
 export default Footer;
